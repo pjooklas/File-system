ed2df8ba57cc59ea3786172c7274039eaf2410f9
pjooklas <pjooklas@users.noreply.github.com>
2022-03-28 18:24:56 +0300

    Read file

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ import { file } from './lib/file.js';
 const petras = {
     name: 'Petras'
 }
-const status = await file.create('users', 'petras.json', petras);
-console.log(status);
-file.read();
+const createFileStatus = await file.create('users', 'petras.json', petras);
+console.log(createFileStatus);
+
+const readFileStatus1 = await file.read('users', 'petras.json');
+console.log(readFileStatus1);
+
 file.update();
-file.read();
+
+const readFileStatus2 = await file.read('users', 'petras.json');
+console.log(readFileStatus2);
+
+file.delete();
+
+const readFileStatus3 = await file.read('users', 'petras.json');
+console.log(readFileStatu3);
