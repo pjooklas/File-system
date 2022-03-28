235078f1020d7e613713db23519bfebd16d6c73a
pjooklas <pjooklas@users.noreply.github.com>
2022-03-28 21:09:46 +0300

    Users uzdavinukai

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ import { file } from './lib/file.js';
 
 console.log('-------------------------');
 const petras = {
-    name: 'Petras'
-}
-const createFileStatus = await file.create('users', 'petras.json', petras);
-console.log(createFileStatus);
-
-const readFileStatus1 = await file.read('users', 'petras.json');
-console.log(readFileStatus1);
-
-const updateFileStatus = await file.update('users', 'petras.json', {
     name: 'Petras',
     age: 99
-});
-console.log(updateFileStatus);
+}
+const maryte = {
+    name: 'Maryte',
+    age: 81
+}
 
-const readFileStatus2 = await file.read('users', 'petras.json');
-console.log(readFileStatus2);
+// const createFileStatus = await file.create('users', 'petras.json', petras);
+// console.log(createFileStatus);
+// const createFileStatus2 = await file.create('users', 'maryte.json', maryte);
+// console.log(createFileStatus2);
 
-const deleteFileStatus = await file.delete('users', 'petras.json');
-console.log(deleteFileStatus);
+// const readFileStatus1 = await file.read('users', 'petras.json');
+// console.log(readFileStatus1);
+
+// const updateFileStatus = await file.update('users', 'petras.json', {
+//     name: 'Petras',
+//     age: 99
+// });
+// console.log(updateFileStatus);
+
+// const readFileStatus2 = await file.read('users', 'petras.json');
+// console.log(readFileStatus2);
+
+// const deleteFileStatus = await file.delete('users', 'petras.json');
+// console.log(deleteFileStatus);
 
 
-const readFileStatus3 = await file.read('users', 'petras.json');
-console.log(readFileStatus3);
+// const readFileStatus3 = await file.read('users', 'petras.json');
+// console.log(readFileStatus3);
+
+const listStatus = await file.list('users');
+console.log(listStatus);
---
+++ users.js
@@ -0,0 +1,30 @@
+// const users = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+
+import { file } from "./lib/file.js";
+
+const users = [
+    { name: 'Petras', age: 99, isMarried: true },
+    { name: 'Maryte', age: 87, isMarried: false },
+    { name: 'Jonas', age: 66, isMarried: false },
+    { name: 'Ona', age: 54, isMarried: true },
+];
+
+
+// 1) Student Petras is 99 years old and is married (petras.json).
+// 2) Student Maryte is 87 years old and is not married.
+
+
+// let i = 0;
+// for (const student of users) {
+//     let jsonas = student.name.toLowerCase() + '.json';
+//     console.log(`${++i}) Student ${student.name} is ${student.age} years old and is${student.isMarried ? '' : ' not'} married (${jsonas})`);
+// }
+
+// Kiekviena user objekta irasyti i atskira faila, kurie turetu buti talpinami `.data/users` folderyje. 
+// Failo pavadinimas atitinka sablona `vardenis.json` (mazosios raides).
+
+let i = 0;
+for (const student of users) {
+    let jsonas = student.name.toLowerCase() + '.json';
+    file.create('users', jsonas, users[i++]);
+}
